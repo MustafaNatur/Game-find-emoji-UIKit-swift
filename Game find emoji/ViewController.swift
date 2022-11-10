778cff3eb3d0fb3c87edabde6605b4e8c5663f7b
Mustafa Natur <mustafa.natur@mail.ru>
2022-11-10 22:51:46 +0300

    Initial Commit

---
+++ Game find emoji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game find emoji
+//
+//  Created by Мустафа Натур on 10.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
