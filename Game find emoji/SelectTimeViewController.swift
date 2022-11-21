057e3a6efbca792593a32c5e096117aa932e6dab
Mustafa Natur <mustafa.natur@mail.ru>
2022-11-21 17:54:16 +0300

    develop: added new view controllers: SettingsTableVC and SelectTimeVC

---
+++ Game find emoji/SelectTimeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SelectTimeViewController.swift
+//  Game find emoji
+//
+//  Created by Мустафа Натур on 21.11.2022.
+//
+
+import UIKit
+
+class SelectTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
